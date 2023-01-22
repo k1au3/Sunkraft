7f76ccafbecc3ade9d0b2ccefcd53c62db8814aa
k1au3 <lindquistconcern@gmail.com>
2023-01-22 06:12:08 -0500

    create admin ui-work in progress

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="icon" href="images/favicon.ico" />
+    <link rel="icon" href="public/favicon/favicon.ico" />
 
     
         <title>Sunkraft</title>
@@ -13,7 +13,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
          
          {{-- favicon   --}}
-         <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml">
+         <link rel="shortcut icon" href="public/favicon/favicon.svg" type="image/svg+xml">
 
           {{-- custom css link --}}
          <link rel="stylesheet" href="/assets/css/style.css">
