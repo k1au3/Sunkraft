e6fb339b9315c76a2c82f8a104401ba875766074
h4ck_the_Gr3at <63366042+Kimosop-Wakanda@users.noreply.github.com>
2023-02-22 10:57:44 +0300

    minor

--- resources/views/admin/Admin-index.blade.php
+++ resources/views/admin/Admin-index.blade.php
@@ -160,7 +160,7 @@
       
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-1">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-              <h1 class="h2">Hello</h1>
+              <h1 class="h2">Dahboard</h1>
               <div class="btn-toolbar mb-2 mb-md-0">
                 <div class="btn-group mr-2">
                   <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
